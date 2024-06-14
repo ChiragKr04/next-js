73cffaa064a91636bfa437b9eea541c996de884f
chirag-gm <chirag@gomassive.org>
2024-06-14 14:03:58 +0530

    nav bar

--- app/user/components/userList.tsx
+++ app/user/components/userList.tsx
@@ -3,33 +3,31 @@ import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function UserList() {
-    async function fetchUserList(): Promise<UserModel[]> {
-        var res = await fetch('https://reqres.in/api/users?page=1&per_page=10')
-            .then(response => response.json());
-        return res.data;
-    }
+  async function fetchUserList(): Promise<UserModel[]> {
+    var res = await fetch('https://reqres.in/api/users?page=1&per_page=10')
+      .then(response => response.json());
+    return res.data;
+  }
 
-    function goToUserDetails() {
-        console.log('User details clicked');
-        // navigate to [userId] page
+  function goToUserDetails() {
+    console.log('User details clicked');
+  }
 
-    }
-
-    let userData: UserModel[] = await fetchUserList();
-    return (
-        <>
-            <Suspense fallback={<div>Loading...</div>}>
-                <ul>
-                    {userData.map((user) => (
-                        <Link href={`user/${user.id}`}>
-                            <li key={user.id}>
-                                <img src={user.avatar} alt={user.first_name} />
-                                <p>{user.first_name} {user.last_name}</p>
-                            </li>
-                        </Link>
-                    ))}
-                </ul>
-            </Suspense>
-        </>
-    );
+  let userData: UserModel[] = await fetchUserList();
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul>
+          {userData.map((user) => (
+            <Link href={`user/${user.id}`} key={user.id}>
+              <li>
+                <img src={user.avatar} alt={user.first_name} />
+                <p>{user.first_name} {user.last_name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </Suspense>
+    </>
+  );
 }
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -4,12 +4,11 @@ import UserList from "./components/userList";
 
 
 export default async function User() {
-    return (
-        <>
-            <h1>User List</h1>
-            <Suspense fallback={<div>Loading...</div>}>
-                <UserList />
-            </Suspense>
-        </>
-    );
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <UserList />
+      </Suspense>
+    </>
+  );
 }
